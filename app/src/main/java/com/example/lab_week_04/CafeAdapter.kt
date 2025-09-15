13e2955c23866a995e76bc10b1c69513ba20683d
Jexy <165495525+Jexxy1517@users.noreply.github.com>
2025-09-16 05:32:23 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -5,20 +5,20 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-private val TABS_FIXED = listOf(
-    R.string.starbucks_title,
-    R.string.janjijiwa_title,
-    R.string.kopikenangan_title
-)
+// Ubah constructor untuk menerima List<String>
+class CafeAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle,
+    private val tabContents: List<String> // Daftar deskripsi
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
 
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
-
+    // getItemCount sekarang berdasarkan ukuran list yang diterima
     override fun getItemCount(): Int {
-        return TABS_FIXED.size
+        return tabContents.size
     }
 
+    // createFragment sekarang mengirimkan konten String langsung
     override fun createFragment(position: Int): Fragment {
-        return CafeDetailFragment()
+        return CafeDetailFragment.newInstance(tabContents[position])
     }
 }
